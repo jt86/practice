503ec9947d46ad3eff9ff9611a75584cecbb4518
Joe Taylor <jt306@sussex.ac.uk>
2015-03-16 14:19:25 +0000

    doing one dataset only

--- Get_Parameter_Settings.py
+++ Get_Parameter_Settings.py
@@ -3,9 +3,16 @@ __author__ = 'jt306'
 values = [0.01, 0.1, 1., 10., 100.]
 values2 = [item / 10000 for item in values]
 
-inputs = ['arcene', 'bankruptcy', 'cancer', 'crx','gisette', 'haberman', 'heart', 'hillvalley', 'ionosphere',
-          'madelon', 'musk1', 'musk2', 'spambase', 'vote', 'wine']
-metrics = ['f', 'c', 'r', 'r2']
+# inputs = ['arcene', 'bankruptcy', 'cancer', 'crx','gisette', 'haberman', 'heart', 'hillvalley', 'ionosphere',
+#           'madelon', 'musk1', 'musk2', 'spambase', 'vote', 'wine']
+
+# metrics = ['f', 'c', 'r', 'r2']
+
+inputs = ['heart']
+
+metrics = ['r2']
+
+
 bottom_n_values = [0, 25, 50]
 peeking_allowed = True
 pattern = '--input {} --num-folds {} --rank-metric {} --prop-priv {} --gamma-multiplier {} --bottom-n-percent {}'
