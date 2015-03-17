9d7e3d495977c5f17646a293c4931623268ee45c
Joe Taylor <jt306@sussex.ac.uk>
2015-03-17 11:34:43 +0000

    changed get param setting module

--- Get_Parameter_Settings.py
+++ Get_Parameter_Settings.py
@@ -3,12 +3,12 @@ __author__ = 'jt306'
 values = [0.01, 0.1, 1., 10., 100.]
 values2 = [item / 10000 for item in values]
 
-# inputs = ['arcene', 'bankruptcy', 'cancer', 'crx','gisette', 'haberman', 'heart', 'hillvalley', 'ionosphere',
-#           'madelon', 'musk1', 'musk2', 'spambase', 'vote', 'wine']
+inputs = ['arcene', 'bankruptcy', 'cancer', 'crx','gisette', 'haberman', 'heart', 'hillvalley', 'ionosphere',
+          'madelon', 'musk1', 'musk2', 'spambase', 'vote', 'wine']
 
 # metrics = ['f', 'c', 'r', 'r2']
 
-inputs = ['heart']
+# inputs = ['heart']
 
 metrics = ['r2']
 
