f8223c0659eea65fcf2cd97cfcd0dcf9afce6b93
Joe Taylor <jt306@sussex.ac.uk>
2015-03-19 18:25:04 +0000

    deleted tmp file

--- tmp.py
+++
@@ -1,4 +0,0 @@
-import logging
-logging.basicConfig(level=logging.INFO,
-                       format="%(asctime)s\t%(module)s.%(funcName)s (line %(lineno)d)\t%(levelname)s : %(message)s")
-logging.info('tewt')
