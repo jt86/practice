9a026f59b92f81a814cbe9c924d54ed10ff481e6
Joe Taylor <jt306@sussex.ac.uk>
2015-04-14 18:21:27 +0100

    All unselected features (below t top) are used as privileged now

--- Get_Parameter_Settings.py
+++ Get_Parameter_Settings.py
@@ -11,13 +11,13 @@ metrics = ['r2']
 
 bottom_n_values = [0]
 peeking_allowed = False
-pattern = '--input {} --num-folds {} --rank-metric {} --prop-priv {} --gamma-multiplier {} --bottom-n-percent {} --cmin {} --cmax {}'
+pattern = '--input {} --num-folds {} --rank-metric {} --initfolds{} --prop-priv {} --gamma-multiplier {} --bottom-n-percent {} --cmin {} --cmax {}'
 
 list = [1,2,3]
 
 for input in inputs:
     for prop_priv in list:
-        txt = pattern.format(input, 5, 'r2', prop_priv, 1, 0,-3,3)
+        txt = pattern.format(input, 5, 'r2', 3, prop_priv, 1, 0,-3,3)
         if peeking_allowed:
             txt += ' --peeking'
         print txt
