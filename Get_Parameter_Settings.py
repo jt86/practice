43b5a6a5c42079fbfd7abd4a643d1895e7469f5f
Joe Taylor <jt306@sussex.ac.uk>
2015-04-23 11:54:48 +0100

    Allowed values for C* hyperparameter to be set independently from C

--- Get_Parameter_Settings.py
+++ Get_Parameter_Settings.py
@@ -11,14 +11,14 @@ metrics = ['r2']
 
 bottom_n_values = [0]
 peeking_allowed = False
-pattern = '--input {} --num-folds 5 --rank-metric r2 --prop-priv 1 --gamma-multiplier 1 --bottom-n-percent 0 --cmin -3 --cmax 3 --numberofcs 7 {} --cstarmin {} --cstarmax {}'
+pattern = '--input {} --num-folds 5 --rank-metric r2 --prop-priv 1 --gamma-multiplier 1 --bottom-n-percent 0 --cmin -6 --cmax 0 --numberofcs 7 {} --cstarmin {} --cstarmax {}'
 
 #--input wine --num-folds 3 --cmin -3 --cmax 3 --numberofcs 2 --rank-metric r2 --prop-priv 1 --gamma-multiplier 1 --bottom-n-percent 0 --peeking
 list = [1,2,3]
 
 for input in inputs:
     for peeking in ['--peeking','']:
-        for cstarmin, cstarmax in [[-3,3],[0,6]]:
+        for cstarmin, cstarmax in [[0,6]]:
             txt = pattern.format(input, peeking, cstarmin, cstarmax)
             # if peeking_allowed:
             #     txt += ' --peeking'
