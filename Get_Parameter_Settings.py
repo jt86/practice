d9d9c15acaa8d35b1d061cfc1912425ffc904de3
Joe Taylor <jt306@sussex.ac.uk>
2015-04-28 15:37:48 +0100

    Shufflesplit now stratified. C in range -5 to 0, C* in 0 to 5. L1 instead of L2 normalised.

--- Get_Parameter_Settings.py
+++ Get_Parameter_Settings.py
@@ -5,7 +5,6 @@ print('getting parameter settings')
 
 inputs = ['heart']#, 'vote', 'wine', 'bankruptcy', 'ionosphere', 'crx','hillvalley', 'cancer',  'musk1','arcene', 'gisette', 'haberman','madelon']
 
-
 bottom_n_values = [0]
 peeking_allowed = False
 pattern = '--input {} --num-folds 5 --rank-metric r2 --prop-priv 1 --gamma-multiplier 1 --bottom-n-percent 0 --cmin -5 --cmax 0 --cstarmin 0 --cstarmax 5 --numberofcs 7 {}'
