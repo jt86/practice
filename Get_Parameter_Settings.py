7599fc9d27740041495f9ef4efc41e57d1942e66
Joe Taylor <jt306@sussex.ac.uk>
2015-04-30 17:38:46 +0100

    Reinstated RFE to get 'top t' features at the beginning using 'topt' parameter switch

--- Get_Parameter_Settings.py
+++ Get_Parameter_Settings.py
@@ -7,7 +7,7 @@ print('getting parameter settings')
 inputs = ['dexter']
 bottom_n_values = [0]
 peeking_allowed = False
-pattern = '--input {} --num-folds 5 --rank-metric r2 --prop-priv 1 --bottom-n-percent 0 --cmin -4 --cmax 1 --cstarmin 0 --cstarmax 5 --numberofcs 6 --kernel rbf {}'
+pattern = '--input {} --num-folds 5 --rank-metric r2 --prop-priv 1 --bottom-n-percent 0 --cmin -4 --cmax 1 --cstarmin 0 --cstarmax 5 --numberofcs 6 --kernel rbf --taket {}'
 
 #--input wine --num-folds 3 --cmin -3 --cmax 3 --numberofcs 2 --rank-metric r2 --prop-priv 1 --gamma-multiplier 1 --bottom-n-percent 0 --peeking
 
