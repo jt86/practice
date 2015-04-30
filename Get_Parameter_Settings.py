c43ca6b2a7a1c46f25bdd0f8e646a93183e91750
Joe Taylor <jt306@sussex.ac.uk>
2015-04-30 16:35:32 +0100

    Code no longer carries out RFE to get 'top t' features at the beginning (uses all features instead).

--- Get_Parameter_Settings.py
+++ Get_Parameter_Settings.py
@@ -7,7 +7,7 @@ inputs = ['madelon','dexter','arcene','gisette','dorothea']
 
 bottom_n_values = [0]
 peeking_allowed = False
-pattern = '--input {} --num-folds 5 --rank-metric r2 --prop-priv 1 --gamma-multiplier 1 --bottom-n-percent 0 --cmin -4 --cmax 1 --cstarmin 0 --cstarmax 5 --numberofcs 6 {}'
+pattern = '--input {} --num-folds 5 --rank-metric r2 --prop-priv 1 --bottom-n-percent 0 --cmin -4 --cmax 1 --cstarmin 0 --cstarmax 5 --numberofcs 6 --kernel rbf {}'
 
 #--input wine --num-folds 3 --cmin -3 --cmax 3 --numberofcs 2 --rank-metric r2 --prop-priv 1 --gamma-multiplier 1 --bottom-n-percent 0 --peeking
 
