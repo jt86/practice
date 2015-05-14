57ea6ee2665c3ca9d4e07bb09186f96f1f753f03
Joe Taylor <jt306@sussex.ac.uk>
2015-05-14 10:43:51 +0100

    CollectBestParams module added, to read the saved best C for RFE from file.

--- Get_Parameter_Settings2.py
+++ Get_Parameter_Settings2.py
@@ -1,14 +1,3 @@
-
-from SingleFold2 import single_fold
-#
-# num_folds=10
-# for fold_num in range(num_folds):
-#     # for dataset_num in range(10):
-#     #     dataset='awa{}'.format(dataset_num)
-#     print fold_num
-#     single_fold(k=fold_num, num_folds=num_folds, take_t=False, bottom_n_percent=0, rank_metric='r2', dataset='awa9', peeking=False, kernel='linear', cmin=0, cmax=4, number_of_cs=5)
-
-
 num_folds=10
 
 for fold_num in range(num_folds):
