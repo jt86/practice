42a540f9e1aad879d873e8137db906cb7638fe53
Joe Taylor <jt306@sussex.ac.uk>
2015-05-14 13:03:57 +0100

    CollectBestParams module added, to read the saved best C for RFE from file.

--- Get_Parameter_Settings2.py
+++ Get_Parameter_Settings2.py
@@ -1,7 +1,8 @@
 num_folds=10
 
-for fold_num in range(num_folds):
-    for dataset_num in range(10):
+
+for dataset_num in range(10):
+    for fold_num in range(num_folds):
         dataset='awa{}'.format(dataset_num)
         print '--k {} --num-folds {} --dataset {} --kernel {} --cmin {} --cmax {} --numberofcs {}'.format(fold_num, 10, dataset, 'linear', 0, 5,6)
 
--- RunGetPics.py
+++ RunGetPics.py
@@ -7,6 +7,6 @@ x_axis_list = [5,10,25,50,75]
 list_of_values = x_axis_list
 
 
-for dataset_num in range(1):
-    cross_validation_folder = '/Volumes/LocalDataHD/jt306/Desktop/Privileged_Data/FixedCandCStar3/awa{}/cross-validation'.format(dataset_num)
+for dataset_num in range(10):
+    cross_validation_folder = '/Volumes/LocalDataHD/jt306/Desktop/Privileged_Data/FixedCandCStar5/awa{}/cross-validation'.format(dataset_num)
     read_from_disk_and_plot(num_folds, cross_validation_folder, list_of_values, x_axis_list)
