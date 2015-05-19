68e9a49fd8f40d163e848d7e05aaa22b9cde9764
Joe Taylor <jt306@sussex.ac.uk>
2015-05-19 20:44:05 +0100

    Fixed standard SVM so it trains directly from RFE

--- SingleFold.py
+++ SingleFold.py
@@ -221,7 +221,7 @@ def get_c_and_cstar(cmin,cmax,number_of_cs, cstarmin=None, cstarmax=None):
     return c_values, cstar_values
 
 #
-for k in range (1,2):
-    single_fold(k=k, num_folds=10, dataset='awa3', peeking=False, kernel='linear', cmin=0, cmax=4, number_of_cs=5)
-#
-#
+# for k in range (1,2):
+#     single_fold(k=k, num_folds=10, dataset='awa3', peeking=False, kernel='linear', cmin=0, cmax=4, number_of_cs=5)
+# #
+# #
