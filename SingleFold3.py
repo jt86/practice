6d3f2e15b69c970c69cf2092209f20f035500091
Joe Taylor <jt306@sussex.ac.uk>
2015-05-23 14:21:03 +0100

    Running for all 5 datasets, using best_c_baseline for SVM+ and C* selected from wider range: 1... 1e-8 )

--- SingleFold3.py
+++ SingleFold3.py
@@ -24,7 +24,7 @@ def single_fold(k, dataset, kernel, cmin,cmax,number_of_cs, cstarmin=None, cstar
         print 'cvalues',c_values
 
         outer_directory = get_full_path('Desktop/Privileged_Data/')
-        output_directory = os.path.join(get_full_path(outer_directory),'{}CV'.format(dataset))
+        output_directory = os.path.join(get_full_path(outer_directory),'{}CV3'.format(dataset))
         if not os.path.exists(output_directory):
             os.makedirs(output_directory)
 
@@ -124,7 +124,7 @@ def single_fold(k, dataset, kernel, cmin,cmax,number_of_cs, cstarmin=None, cstar
             privileged_features_training=all_training[:,np.invert(rfe.support_)].copy()
 
             c_svm_plus=best_C_baseline
-            c_star_values = [1., 0.1, 0.01, 0.001, 0.0001]
+            c_star_values = [1., 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.00000001]
             print 'getting best c star'
             c_star_svm_plus=get_best_Cstar(normal_features_training,training_labels, privileged_features_training, c_svm_plus, c_star_values)
             print 'c star', c_star_svm_plus, '\n'
