77e6362062e623f4e65b4cf2b7cc3eba886f7797
Joe Taylor <jt306@sussex.ac.uk>
2015-05-23 15:12:36 +0100

    Madelon only - trying C* in reverse order

--- Get_Parameter_Settings2.py
+++ Get_Parameter_Settings2.py
@@ -7,7 +7,7 @@ num_folds=10
 #         dataset='awa{}'.format(dataset_num)
 #         print '--k {} --num-folds {} --dataset {} --kernel {} --cmin {} --cmax {} --numberofcs {}'.format(fold_num, 10, dataset, 'linear', 0, 4,5)
 
-
-for dataset in ['gisette','madelon','arcene','dorothea','dexter']:
+# for dataset in ['gisette','madelon','arcene','dorothea','dexter']:
+for dataset in ['madelon']:
     for fold_num in range(1,11):
         print '--k {}  --dataset {} --kernel {} --cmin {} --cmax {} --numberofcs {}'.format(fold_num, dataset, 'linear', 0, 7,8)
