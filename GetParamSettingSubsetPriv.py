b98461e8c4ec195c7fc011b0f9d328f61c8265d6
Joe Taylor <jt306@sussex.ac.uk>
2015-05-31 20:21:22 +0100

    Running for gisette and arcene  and dexter with subset of privileged features only

--- GetParamSettingSubsetPriv.py
+++ GetParamSettingSubsetPriv.py
@@ -1,7 +1,7 @@
 num_folds=10
 subset_sizes = range(10,101,10)
-print subset_sizes
-for dataset in ['gisette','arcene','mushroom','dexter']:
+
+for dataset in ['mushroom','gisette','arcene','dexter']:
     for fold_num in range(1,11):
         for subset_of_priv in subset_sizes:
             print '--k {} --subsetofpriv {} --dataset {} --kernel {} --cmin {} --cmax {} --numberofcs {}'.format(fold_num, subset_of_priv, dataset, 'linear', 0, 4,5)
