e5552c7c1eef282e066d0f5cb66d3f17663ca528
Joe Taylor <jt306@sussex.ac.uk>
2015-06-01 09:30:37 +0100

    SUBSET OF PRIVILEGED FINALLY WORKING!!! - AWA datasets

--- GetParamSettingSubsetPriv.py
+++ GetParamSettingSubsetPriv.py
@@ -1,8 +1,9 @@
 num_folds=10
 subset_sizes = range(10,101,10)
 
-for dataset in ['mushroom','gisette','arcene','dexter']:
+for awanum in range(10):
     for fold_num in range(1,11):
         for subset_of_priv in subset_sizes:
+            dataset='awa{}'.format(awanum)
             print '--k {} --subsetofpriv {} --dataset {} --kernel {} --cmin {} --cmax {} --numberofcs {}'.format(fold_num, subset_of_priv, dataset, 'linear', 0, 4,5)
 
