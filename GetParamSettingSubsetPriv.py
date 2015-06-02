2c24326cde6f1ccfb2234f8a2368ff2cb47e2177
Joe Taylor <jt306@sussex.ac.uk>
2015-06-02 20:09:59 +0100

    rerunning mushroom original experiment

--- GetParamSettingSubsetPriv.py
+++ GetParamSettingSubsetPriv.py
@@ -5,5 +5,5 @@ for awanum in range(10):
     for fold_num in range(1,11):
         for subset_of_priv in subset_sizes:
             dataset='awa{}'.format(awanum)
-            print '--k {} --subsetofpriv {} --dataset {} --kernel {} --cmin {} --cmax {} --numberofcs {}'.format(fold_num, subset_of_priv, dataset, 'linear', 0, 4,5)
+            print '--k {} --per--subsetofpriv {} --dataset {} --kernel {} --cmin {} --cmax {} --numberofcs {}'.format(fold_num, subset_of_priv, dataset, 'linear', 0, 4,5)
 
