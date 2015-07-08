7f01aaf3beaa56f81a53c97ebe97b4977af4f17e
Joe Taylor <jt306@sussex.ac.uk>
2015-07-08 15:38:38 +0100

    Commiting everything before 2to3

--- Get_Mean.py
+++ Get_Mean.py
@@ -8,18 +8,17 @@ list_of_lists = [[5,5,5],[43,3],[3,1,5,6,1]]
 def get_mean_from(list_of_lists):
     results = []
     izipped = list(itertools.izip_longest(*list_of_lists, fillvalue=np.nan))
-    print 'getting mean'
-    print 'izipped',izipped
+    # print 'getting mean'
+    # print 'izipped',izipped
     for int in range(len(izipped)):
         results.append(np.nanmean(izipped[int]))
     return results
 
 def get_error_from(list_of_lists):
-    results = []
+    errors = []
     izipped = list(itertools.izip_longest(*list_of_lists, fillvalue=np.nan))
-    print 'getting error'
-    print 'izipped',izipped
+    # print 'getting error'
+    # print 'izipped',izipped
     for int in range(len(izipped)):
-        # results.append(np.nanstd(izipped[int]))
-        results.append(stats.sem(izipped[int]))
-    return results
+        errors.append(stats.sem(izipped[int]))
+    return errors
