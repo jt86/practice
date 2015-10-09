af7e30d21c2a00209f9ccd2cb96b10adf0f98e81
Joe Taylor <jt306@sussex.ac.uk>
2015-10-09 14:40:38 +0100

    Running on FEAT SELECTION datasets (66% train, 33% test) - STEPSIZE=1000 -BOTTOM 50

--- SingleFold3.py
+++ SingleFold3.py
@@ -13,7 +13,7 @@ def single_fold(k, topk, dataset,datasetnum, kernel, cmin,cmax,number_of_cs, per
         stepsize=1000
         np.random.seed(k)
         c_values = np.logspace(cmin,cmax,number_of_cs)
-        outer_directory = get_full_path('Desktop/Privileged_Data/OCTOBER7/66-33/TOP50')
+        outer_directory = get_full_path('Desktop/Privileged_Data/OCTOBER7/66-33/BOTTOM50')
         # Check if output directory exists and make it if necessary
         output_directory = os.path.join(get_full_path(outer_directory),'not-fixedC-33-66-{}-{}-RFE-baseline-step={}-percent_of_priv={}'.format(dataset,datasetnum,stepsize,percent_of_priv))
         print (output_directory)
@@ -122,7 +122,7 @@ def single_fold(k, topk, dataset,datasetnum, kernel, cmin,cmax,number_of_cs, per
         num_of_priv_feats=percent_of_priv*privileged_features_training.shape[1]//100
         print('number to take', num_of_priv_feats)
 
-        privileged_features_training = privileged_features_training[:,-num_of_priv_feats:]
+        privileged_features_training = privileged_features_training[:,:num_of_priv_feats]
         print (privileged_features_training.shape)
 
         c_svm_plus=best_C_baseline
