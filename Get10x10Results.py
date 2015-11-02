Running on TECH datasets (10 x 4 fold)- stepsize=10% - 300 only - fixed Cs, fine-grained search for C* - original normalisation code reinstated

Get10x10Results.py
@@ -22,7 +22,7 @@ for dataset_num in range(49):
     all_folds_baseline=[]
     all_folds_SVM,all_folds_LUPI = [],[]
     for outer_fold in range (10):
-        output_directory = (get_full_path('Desktop/Privileged_Data/10x4_finegrained_normscorrected/fixedCandCstar-10fold-tech-{}-RFE-baseline-step=0.1-percent_of_priv=100/cross-validation{}'.format(dataset_num,outer_fold)))
+        output_directory = (get_full_path('Desktop/Privileged_Data/10x4_finegrained_nonorms/fixedCandCstar-10fold-tech-{}-RFE-baseline-step=0.1-percent_of_priv=100/cross-validation{}'.format(dataset_num,outer_fold)))
         with open(os.path.join(output_directory,'baseline.csv'),'r') as baseline_file:
             baseline_i_list = baseline_file.readline().split(',')[:-1]
             baseline_i_list = list(map(float, baseline_i_list))
@@ -56,51 +56,51 @@ rfe_error_bars = list(stats.sem(list_of_300_rfe,axis=1))
 lupi_error_bars = list(stats.sem(list_of_300_lupi,axis=1))
 
 ######################################
-x = list(range(49))
-y = list(range(49))
+# x = list(range(49))
+# y = list(range(49))
+#
+#
+# list_of_baselines2=[]
+# list_of_300_rfe2=[]
+# list_of_300_lupi2=[]
+#
+# for dataset_num in range(49):
+#     print ('doing dataset',dataset_num)
+#     all_folds_baseline=[]
+#     all_folds_SVM,all_folds_LUPI = [],[]
+#     for outer_fold in range (10):
+#         output_directory = (get_full_path('Desktop/Privileged_Data/TechSlice-10x4-finegrained/fixedCandCstar-10fold-tech-{}-RFE-baseline-step=0.1-percent_of_priv=100/cross-validation{}'.format(dataset_num,outer_fold)))
+#         with open(os.path.join(output_directory,'baseline.csv'),'r') as baseline_file:
+#             baseline_i_list = baseline_file.readline().split(',')[:-1]
+#             baseline_i_list = list(map(float, baseline_i_list))
+#             # print (baseline_i_list)
+#             all_folds_baseline+=baseline_i_list
+#             # print (len(list_of_baselines))
+#         for inner_fold in range(4):
+#             with open(os.path.join(output_directory,'svm-{}-{}.csv').format(inner_fold,300),'r') as cv_svm_file:
+#                 svm_score = float(cv_svm_file.readline().split(',')[0])
+#                 all_folds_SVM+=[svm_score]
+#             with open(os.path.join(output_directory,'lupi-{}-{}.csv').format(inner_fold,300),'r') as cv_lupi_file:
+#                 lupi_score = float(cv_lupi_file.readline().split(',')[0])
+#                 # print (outer_fold,inner_fold,svm_score)
+#                 all_folds_LUPI+=[lupi_score]
+#     list_of_baselines2.append(all_folds_baseline)
+#     list_of_300_rfe2.append(all_folds_SVM)
+#     list_of_300_lupi2.append(all_folds_LUPI)
 
 
-list_of_baselines2=[]
-list_of_300_rfe2=[]
-list_of_300_lupi2=[]
+# list_of_baseline_errors2 = np.array([1-mean for mean in np.mean(list_of_baselines2,axis=1)])
+# list_of_rfe_errors2 = np.array([1-mean for mean in np.mean(list_of_300_rfe2,axis=1)])
+# list_of_lupi_errors2 = np.array([1-mean for mean in np.mean(list_of_300_lupi2,axis=1)])
 
-for dataset_num in range(49):
-    print ('doing dataset',dataset_num)
-    all_folds_baseline=[]
-    all_folds_SVM,all_folds_LUPI = [],[]
-    for outer_fold in range (10):
-        output_directory = (get_full_path('Desktop/Privileged_Data/TechSlice-10x4-finegrained/fixedCandCstar-10fold-tech-{}-RFE-baseline-step=0.1-percent_of_priv=100/cross-validation{}'.format(dataset_num,outer_fold)))
-        with open(os.path.join(output_directory,'baseline.csv'),'r') as baseline_file:
-            baseline_i_list = baseline_file.readline().split(',')[:-1]
-            baseline_i_list = list(map(float, baseline_i_list))
-            # print (baseline_i_list)
-            all_folds_baseline+=baseline_i_list
-            # print (len(list_of_baselines))
-        for inner_fold in range(4):
-            with open(os.path.join(output_directory,'svm-{}-{}.csv').format(inner_fold,300),'r') as cv_svm_file:
-                svm_score = float(cv_svm_file.readline().split(',')[0])
-                all_folds_SVM+=[svm_score]
-            with open(os.path.join(output_directory,'lupi-{}-{}.csv').format(inner_fold,300),'r') as cv_lupi_file:
-                lupi_score = float(cv_lupi_file.readline().split(',')[0])
-                # print (outer_fold,inner_fold,svm_score)
-                all_folds_LUPI+=[lupi_score]
-    list_of_baselines2.append(all_folds_baseline)
-    list_of_300_rfe2.append(all_folds_SVM)
-    list_of_300_lupi2.append(all_folds_LUPI)
-
-
-list_of_baseline_errors2 = np.array([1-mean for mean in np.mean(list_of_baselines2,axis=1)])
-list_of_rfe_errors2 = np.array([1-mean for mean in np.mean(list_of_300_rfe2,axis=1)])
-list_of_lupi_errors2 = np.array([1-mean for mean in np.mean(list_of_300_lupi2,axis=1)])
-
-print ('baseline sorted',np.argsort(list_of_baseline_errors2))
-list_of_rfe_errors2 = list_of_rfe_errors2[np.argsort(list_of_baseline_errors2)]
-list_of_lupi_errors2 = list_of_lupi_errors2[np.argsort(list_of_baseline_errors2)]
-list_of_baseline_errors2 = list_of_baseline_errors2[np.argsort(list_of_baseline_errors2)]
-
-baseline_error_bars2=list(stats.sem(list_of_baselines2,axis=1))
-rfe_error_bars2 = list(stats.sem(list_of_300_rfe2,axis=1))
-lupi_error_bars2 = list(stats.sem(list_of_300_lupi2,axis=1))
+# print ('baseline sorted',np.argsort(list_of_baseline_errors2))
+# list_of_rfe_errors2 = list_of_rfe_errors2[np.argsort(list_of_baseline_errors2)]
+# list_of_lupi_errors2 = list_of_lupi_errors2[np.argsort(list_of_baseline_errors2)]
+# list_of_baseline_errors2 = list_of_baseline_errors2[np.argsort(list_of_baseline_errors2)]
+#
+# baseline_error_bars2=list(stats.sem(list_of_baselines2,axis=1))
+# rfe_error_bars2 = list(stats.sem(list_of_300_rfe2,axis=1))
+# lupi_error_bars2 = list(stats.sem(list_of_300_lupi2,axis=1))
 
 
 #######################################
@@ -112,14 +112,14 @@ plt.errorbar(list(range(49)), list_of_rfe_errors, yerr = rfe_error_bars, c='b', 
 plt.errorbar(list(range(49)), list_of_lupi_errors, yerr = lupi_error_bars, c='r', label='LUPI - top 300, rest privileged corrected')
 
 
-plt.errorbar(list(range(49)), list_of_baseline_errors2, yerr = baseline_error_bars2, c='cyan', label='All features')
-plt.errorbar(list(range(49)), list_of_rfe_errors2, yerr = rfe_error_bars2, c='k', label='RFE - top 300 features')
-plt.errorbar(list(range(49)), list_of_lupi_errors2, yerr = lupi_error_bars2, c='magenta', label='LUPI - top 300, rest privileged')
+# plt.errorbar(list(range(49)), list_of_baseline_errors2, yerr = baseline_error_bars2, c='cyan', label='All features')
+# plt.errorbar(list(range(49)), list_of_rfe_errors2, yerr = rfe_error_bars2, c='k', label='RFE - top 300 features')
+# plt.errorbar(list(range(49)), list_of_lupi_errors2, yerr = lupi_error_bars2, c='magenta', label='LUPI - top 300, rest privileged')
 
 
 fig.suptitle('TechTC-300 - Error rates', fontsize=20)
 plt.legend(loc='best')#bbox_to_anchor=(0.6, 1))#([line1,line2],['All features',['RFE - top 300 features']])
-fig.savefig('10x4_norms')
+fig.savefig('10x4_no_normalisation')
 plt.show()
 
 

GetSingleFoldData.py
@@ -93,8 +93,8 @@ def get_train_and_test_this_fold(dataset,datasetnum,k, skf_seed):	#N,test_N per 
     #
     # normaliser = Normalizer(norm='l1')
     # test_data=normaliser.fit_transform(test_data)
-    # train_data = train_data/np.apply_along_axis(lambda row:np.linalg.norm(row,ord=1), 1, train_data).reshape(-1,1)
-    # test_data = test_data/np.apply_along_axis(lambda row:np.linalg.norm(row,ord=1), 1, test_data).reshape(-1,1)
+    train_data = train_data/np.apply_along_axis(lambda row:np.linalg.norm(row,ord=1), 1, train_data).reshape(-1,1)
+    test_data = test_data/np.apply_along_axis(lambda row:np.linalg.norm(row,ord=1), 1, test_data).reshape(-1,1)
     print ('train data shape', train_data.shape, 'test data shape', test_data.shape)
     return np.asarray(train_data), np.asarray(test_data), np.asarray(train_labels), np.asarray(test_labels)
 

SingleFoldSlice.py
@@ -15,7 +15,7 @@ def single_fold(k, topk, dataset,datasetnum, kernel, cmin,cmax,number_of_cs, skf
         stepsize=0.1
         np.random.seed(k)
         c_values = np.logspace(cmin,cmax,number_of_cs)
-        outer_directory = get_full_path('Desktop/Privileged_Data/10x4_finegrained_nonorms/')
+        outer_directory = get_full_path('Desktop/Privileged_Data/10x4_finegrained_oldstylenorms/')
         output_directory = os.path.join(get_full_path(outer_directory),'fixedCandCstar-10fold-{}-{}-RFE-baseline-step={}-percent_of_priv={}'.format(dataset,datasetnum,stepsize,percent_of_priv))
         print (output_directory)
         try: