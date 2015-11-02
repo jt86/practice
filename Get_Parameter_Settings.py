e504a3445330f83386ab3479d9db34d35b1301a0
Joe Taylor <jt306@sussex.ac.uk>
2015-11-02 18:15:05 +0000

    Running on TECH datasets (10 x 4 fold)- stepsize=10% - 300 only - fixed Cs, fine-grained search for C* - norm removed

--- Get_Parameter_Settings.py
+++ Get_Parameter_Settings.py
@@ -1,18 +1,14 @@
 #hello hello
 dataset='tech'
 
-# for seed in range (10):  #4
-#     for top_k in [300]:#,500]:#100,200,400,600,700,800,900,1000]:
-#         for datasetnum in range (49): #5
-#             for fold_num in range(4): #0
-#                 print('--k {} --topk {} --dataset {} --datasetnum {} --kernel {} --cmin {} --cmax {} --numberofcs {} --skfseed {}'.format(fold_num, top_k, dataset, datasetnum, 'linear', 0, 4,5, seed))
-#
-#
-# for seed in range (10):  #4
-#     for top_k in [300]:#,500]:#100,200,400,600,700,800,900,1000]:
-#         for datasetnum in range (49): #5
-#             for fold_num in range(4): #0
-print('--k 0 --topk 300 --dataset "tech" --datasetnum 5  --kernel "linear" --cmin 0 --cmax 4 --numberofcs 5 --skfseed 4')
+for seed in range (10):  #4
+    for top_k in [300]:#,500]:#100,200,400,600,700,800,900,1000]:
+        for datasetnum in range (49): #5
+            for fold_num in range(4): #0
+                print('--k {} --topk {} --dataset {} --datasetnum {} --kernel {} --cmin {} --cmax {} --numberofcs {} --skfseed {}'.format(fold_num, top_k, dataset, datasetnum, 'linear', 0, 4,5, seed))
+
+
+# print('--k 0 --topk 300 --dataset "tech" --datasetnum 5  --kernel "linear" --cmin 0 --cmax 4 --numberofcs 5 --skfseed 4')
 
 
 
