798862567be51c86c1a3f4633923855b1d619d49
Joe Taylor <jt306@sussex.ac.uk>
2015-12-10 18:24:17 +0000

    Investigating values of features

--- GetMI.py
+++ GetMI.py
@@ -21,7 +21,7 @@ def single_fold(k, topk, dataset,datasetnum, kernel, cmin,cmax,number_of_cs, skf
         c_values = np.logspace(cmin,cmax,number_of_cs)
         print('cvalues',c_values)
 
-        output_directory = get_full_path(('Desktop/Privileged_Data/GetScore-{}{}-{}to{}-{}-{}').format(dataset,datasetnum,cmin,cmax,stepsize,topk))
+        output_directory = get_full_path(('Desktop/Privileged_Data/GetCoefficients-{}{}-{}to{}-{}-{}').format(dataset,datasetnum,cmin,cmax,stepsize,topk))
 
         print (output_directory)
         try:
@@ -50,47 +50,88 @@ def single_fold(k, topk, dataset,datasetnum, kernel, cmin,cmax,number_of_cs, skf
         best_rfe_param = get_best_RFE_C(all_training,training_labels, c_values, n_top_feats,stepsize,output_directory,datasetnum,topk)
         print('best rfe param', best_rfe_param)
 
+
+        ########## GET INITIAL WEIGHTS
+
+        svc = SVC(C=best_rfe_param, kernel="linear", random_state=k)
+        svc.fit(all_training, training_labels)
+        all_coefficients = svc.coef_
+        print ('coefficients 1',all_coefficients)
+        print ('coefficients 1',all_coefficients.shape)
         ###########  CARRY OUT RFE, GET ACCURACY
 
         svc = SVC(C=best_rfe_param, kernel="linear", random_state=k)
-        rfe = RFE(estimator=svc, n_features_to_select=n_top_feats, step=stepsize)
+        rfe = RFE(estimator=svc, n_features_to_select=n_top_feats, step=stepsize, verbose=10)
         print ('rfe step size',rfe.step)
         rfe.fit(all_training, training_labels)
         print (all_testing.shape,testing_labels.shape)
         print ('num of chosen feats',sum(x == 1 for x in rfe.support_))
 
-        best_n_mask = rfe.support_
-        priv_feats_all = all_features[:,np.invert(rfe.support_)].copy()
-        normal_feats_all = all_features[:,best_n_mask].copy()
 
-        priv_feats_train = all_training[:,np.invert(rfe.support_)].copy()
-        normal_feats_train = all_training[:,best_n_mask].copy()
+        print ('coefficients',rfe.estimator_.coef_)
+        print ('coefficients',rfe.estimator_.coef_.shape)
 
-        priv_feats_test = all_testing[:,np.invert(rfe.support_)].copy()
-        normal_feats_test = all_testing[:,best_n_mask].copy()
 
-        ########### WRITE SCORES FOR EACH FEATURE TO FILE
 
-        with open(os.path.join(output_directory,'normal-all-f_classif-{}-{}.csv'.format(k,skfseed)),'a') as normal_chi2_file:
-                for item in (f_classif(normal_feats_all, all_labels)[0]):
-                        normal_chi2_file.write(str(item)+',')
-        with open(os.path.join(output_directory,'priv-all-f_classif-{}-{}.csv'.format(k,skfseed)),'a') as priv_chi2_file:
-                for item in (f_classif(priv_feats_all, all_labels)[0]):
-                        priv_chi2_file.write(str(item)+',')
+        ########## USE RFE TO GET UN/SELECTED FEATURES; GET COEFFICIENTS FOR THESE
+        priv_coefficients = all_coefficients[:,np.invert(rfe.support_)].copy()
+        print('priv coef shape',priv_coefficients.shape)
+        print('priv coef',priv_coefficients)
 
-        with open(os.path.join(output_directory,'normal-train-f_classif-{}-{}.csv'.format(k,skfseed)),'a') as normal_chi2_file:
-                for item in (f_classif(normal_feats_train, training_labels)[0]):
-                        normal_chi2_file.write(str(item)+',')
-        with open(os.path.join(output_directory,'priv-train-f_classif-{}-{}.csv'.format(k,skfseed)),'a') as priv_chi2_file:
-                for item in (f_classif(priv_feats_train, training_labels)[0]):
-                        priv_chi2_file.write(str(item)+',')
+        normal_coefficients = all_coefficients[:,rfe.support_].copy()
+        print ('normal coef shape', normal_coefficients.shape)
+        print ('normal coef', normal_coefficients)
 
-        with open(os.path.join(output_directory,'normal-test-f_classif-{}-{}.csv'.format(k,skfseed)),'a') as normal_chi2_file:
-                for item in (f_classif(normal_feats_test, testing_labels)[0]):
-                        normal_chi2_file.write(str(item)+',')
-        with open(os.path.join(output_directory,'priv-test-f_classif-{}-{}.csv'.format(k,skfseed)),'a') as priv_chi2_file:
-                for item in (f_classif(priv_feats_test, testing_labels)[0]):
-                        priv_chi2_file.write(str(item)+',')
+
+        with open(os.path.join(output_directory,'normal-coefficients-{}-{}.csv'.format(k,skfseed)),'a') as normal_coefficients_file:
+                for item in normal_coefficients[0]:
+                        normal_coefficients_file.write(str(item)+',')
+        with open(os.path.join(output_directory,'priv-coefficients-{}-{}.csv'.format(k,skfseed)),'a') as priv_coefficients_file:
+                for item in priv_coefficients[0]:
+                        priv_coefficients_file.write(str(item)+',')
+
+
+
+        # with open(os.path.join(output_directory,'normal-all-f_classif-{}-{}.csv'.format(k,skfseed)),'a') as normal_chi2_file:
+        #         for item in (f_classif(normal_feats_all, all_labels)[0]):
+        #                 normal_chi2_file.write(str(item)+',')
+        # with open(os.path.join(output_directory,'priv-all-f_classif-{}-{}.csv'.format(k,skfseed)),'a') as priv_chi2_file:
+        #         for item in (f_classif(priv_feats_all, all_labels)[0]):
+        #                 priv_chi2_file.write(str(item)+',')
+        #
+        #
+        # best_n_mask = rfe.support_
+        # priv_feats_all = all_features[:,np.invert(rfe.support_)].copy()
+        # normal_feats_all = all_features[:,best_n_mask].copy()
+        #
+        # priv_feats_train = all_training[:,np.invert(rfe.support_)].copy()
+        # normal_feats_train = all_training[:,best_n_mask].copy()
+        #
+        # priv_feats_test = all_testing[:,np.invert(rfe.support_)].copy()
+        # normal_feats_test = all_testing[:,best_n_mask].copy()
+        #
+        # ########### WRITE SCORES FOR EACH FEATURE TO FILE
+        #
+        # with open(os.path.join(output_directory,'normal-all-f_classif-{}-{}.csv'.format(k,skfseed)),'a') as normal_chi2_file:
+        #         for item in (f_classif(normal_feats_all, all_labels)[0]):
+        #                 normal_chi2_file.write(str(item)+',')
+        # with open(os.path.join(output_directory,'priv-all-f_classif-{}-{}.csv'.format(k,skfseed)),'a') as priv_chi2_file:
+        #         for item in (f_classif(priv_feats_all, all_labels)[0]):
+        #                 priv_chi2_file.write(str(item)+',')
+        #
+        # with open(os.path.join(output_directory,'normal-train-f_classif-{}-{}.csv'.format(k,skfseed)),'a') as normal_chi2_file:
+        #         for item in (f_classif(normal_feats_train, training_labels)[0]):
+        #                 normal_chi2_file.write(str(item)+',')
+        # with open(os.path.join(output_directory,'priv-train-f_classif-{}-{}.csv'.format(k,skfseed)),'a') as priv_chi2_file:
+        #         for item in (f_classif(priv_feats_train, training_labels)[0]):
+        #                 priv_chi2_file.write(str(item)+',')
+        #
+        # with open(os.path.join(output_directory,'normal-test-f_classif-{}-{}.csv'.format(k,skfseed)),'a') as normal_chi2_file:
+        #         for item in (f_classif(normal_feats_test, testing_labels)[0]):
+        #                 normal_chi2_file.write(str(item)+',')
+        # with open(os.path.join(output_directory,'priv-test-f_classif-{}-{}.csv'.format(k,skfseed)),'a') as priv_chi2_file:
+        #         for item in (f_classif(priv_feats_test, testing_labels)[0]):
+        #                 priv_chi2_file.write(str(item)+',')
 
 
 def get_random_array(num_instances,num_feats):
