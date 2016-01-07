407c62f1780f97bb10b52c447d438fabf382fcf2
Joe Taylor <jt306@sussex.ac.uk>
2016-01-07 19:00:30 +0000

    Comparing performance with 100% training instances

--- practice2.py
+++ practice2.py
@@ -6,3 +6,27 @@ for i in range(64):
         print (True)
         random_bits |= 1 <<i
 print (random_bits)
+
+
+def sort_priority(values,group):
+    def helper(x):
+        if x in group:
+            return(0,x)
+        return (1,x)
+    values.sort(key=helper)
+
+
+numbers=[8,3,1,2,4,5,7,6]
+group = {7,2,5,3,7}
+print( sort_priority(numbers,group))
+print(group)
+print(numbers)
+
+def generate_power_func(n):
+    print ("id(n): %X" % id(n))
+    def nth_power(x):
+        return x**n
+    print ("id(nth_power): %X" % id(nth_power))
+    return nth_power
+
+generate_power_func(5)
