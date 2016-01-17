82716b5b6ef1b10fc51a9f12bf27fc78e564c4fe
Joe Taylor <jt306@sussex.ac.uk>
2016-01-17 17:03:38 +0000

    Fixed randomisation - 5000 - rfe - awa
    Also: top 5% and top 50% data - tech,awa

--- Get_Parameter_Settings.py
+++ Get_Parameter_Settings.py
@@ -29,8 +29,17 @@ dataset='tech'
 
 for top_k in [300,500]:#,500]:#100,200,400,600,700,800,900,1000]:
     for percentofpriv in [5,50]:
-        for datasetnum in range (1): #5
+        for datasetnum in range (49): #5
             for seed in range (10):
+                for fold_num in range(10): #0
+                    print('--k {} --topk {} --dataset {} --datasetnum {} --kernel {} --cmin {} --cmax {} --numberofcs {} --skfseed {} --percentofpriv {} --percentageofinstances {}'.format(fold_num, top_k, dataset, datasetnum, 'linear', -3, 3, 7, seed, percentofpriv, 100))
+                    count+=1
+
+dataset='awa'
+for percentofpriv in [5,50]:
+    for seed in range (10):  #4
+        for top_k in [5000]:
+            for datasetnum in range (10): #5
                 for fold_num in range(10): #0
                     print('--k {} --topk {} --dataset {} --datasetnum {} --kernel {} --cmin {} --cmax {} --numberofcs {} --skfseed {} --percentofpriv {} --percentageofinstances {}'.format(fold_num, top_k, dataset, datasetnum, 'linear', -3, 3, 7, seed, percentofpriv, 100))
                     count+=1
