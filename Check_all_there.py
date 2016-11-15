512e7b601b90b07167bf50f32a08adf5350a3500
Joe Taylor <jt306@sussex.ac.uk>
2016-11-15 11:17:11 +0000

    Changed Get_Parameter_Settings to run just the 18 failed jobs

--- Check_all_there.py
+++ Check_all_there.py
@@ -1,6 +1,6 @@
 import os
 path = '/Volumes/LocalDataHD/j/jt/jt306/Desktop/results/GPC_conf'
-
+count=0
 for num_selected in [300,500]:
     for datasetnum in range(49):
         for seed in range(10):
