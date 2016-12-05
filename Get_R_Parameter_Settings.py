ad93ac434b6a731d9537f03be05c6911dd7c43b5
Joe Taylor <jt306@sussex.ac.uk>
2016-12-05 11:19:33 +0000

    Running R

--- Get_R_Parameter_Settings.py
+++ Get_R_Parameter_Settings.py
@@ -5,16 +5,16 @@ Generate list of parameters. Called by RunExperiment.py when running on cluster
 from SaveDataForR import save_instance_and_feature_indices_for_R, save_dataset_for_R
 count=0
 
-dataset='tech'
-for top_k in [500]:#,500]:#,500]:#100,200,400,600,700,800,900,1000]:
-    for take_top_t in ['top']:#,'bottom']:
-        # for percentofpriv in [10,20,30,40,50,60,70,80,90]:
-        for percentofpriv in [100]:
-            for datasetnum in range (49,92):
-                # save_dataset_for_R(datasetnum)
-                for seed in range (10):
-                    for fold_num in range(10): #0
-                        print('--k {} --topk {} --dataset {} --datasetnum {} --kernel {} --cmin {} --cmax {} --numberofcs {} --skfseed {} --percentofpriv {} --percentageofinstances {} --taketopt {}'.format(fold_num, top_k, dataset, datasetnum, 'linear', -3, 3, 7, seed, percentofpriv, 100, take_top_t))
+# dataset='tech'
+# for top_k in [300,500]:#,500]:#,500]:#100,200,400,600,700,800,900,1000]:
+#     for take_top_t in ['top']:#,'bottom']:
+#         # for percentofpriv in [10,20,30,40,50,60,70,80,90]:
+#         for percentofpriv in [100]:
+#             for datasetnum in range (49,92):
+#                 # save_dataset_for_R(datasetnum)
+#                 for seed in range (10):
+#                     for fold_num in range(10): #0
+#                         print('--k {} --topk {} --dataset {} --datasetnum {} --kernel {} --cmin {} --cmax {} --numberofcs {} --skfseed {} --percentofpriv {} --percentageofinstances {} --taketopt {}'.format(fold_num, top_k, dataset, datasetnum, 'linear', -3, 3, 7, seed, percentofpriv, 100, take_top_t))
 #                         count+=1
 # print(count)
 #                         save_instance_and_feature_indices_for_R(k=fold_num, dataset=dataset, topk=top_k,
@@ -35,14 +35,14 @@ for top_k in [500]:#,500]:#,500]:#100,200,400,600,700,800,900,1000]:
 #                         count+=1
 
 # CVPR2016_Rcode jt306$ R 500 39 3 4 --no-save < run_scripts.R
-# for number_selected in [300, 500]:
-#     for datasetnum in range(49):  # 5
-#         for seed in range(10):
-#             for fold_num in range(10):
-#                 # print('R {} {} {} {} --no-save < run_scripts.R'.format(top_k,datasetnum,seed,fold_num))
-#                 print('{} {} {} {}'.format(number_selected, datasetnum, seed, fold_num))
-#                 count+=1
-# print (count)
+for number_selected in [300, 500]:
+    for datasetnum in range(49,295):  # 5
+        for seed in range(10):
+            for fold_num in range(10):
+                # print('R {} {} {} {} --no-save < run_scripts.R'.format(top_k,datasetnum,seed,fold_num))
+                print('{} {} {} {}'.format(number_selected, datasetnum, seed, fold_num))
+                count+=1
+print (count)
 
 # print ("500 7 0 4")
 # print ("500 7 0 9")
