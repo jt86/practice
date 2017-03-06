e00f30e2b13e882519ba1384b55203204690fde3
Joe Taylor <jt306@sussex.ac.uk>
2017-03-06 19:46:13 +0000

    Running LUFe on 295 tech, Using 100, 10%  of priv features - UNIVARIATE (ANOVA, CHI2)

--- SingleFoldSliceUnivariate.py
+++ SingleFoldSliceUnivariate.py
@@ -28,7 +28,59 @@ from sklearn import preprocessing
 from sklearn.feature_selection import SelectPercentile, f_classif, chi2, mutual_info_classif, VarianceThreshold
 # print (PYTHONPATH)
 
+def get_anova_ordered_indices(all_training,training_labels):
 
+        # get array of scores in the same order as original features
+        selector = SelectPercentile(f_classif, percentile=100)
+        selector.fit(all_training, training_labels)
+        scores = selector.scores_
+
+        #sort the scores (small to big)
+        sorted_indices_small_to_big = np.argsort(scores)
+        sorted_scores = scores[sorted_indices_small_to_big]
+
+        # nan indices are at the end. Reverse the score array (big to small) but leave these at the end
+        nan_indices = np.argwhere(np.isnan(scores)).flatten()
+        non_nan_indices = sorted_indices_small_to_big[:-len(nan_indices)]
+        sorted_indices_big_to_small = non_nan_indices[::-1]
+        ordered_indices = np.array(np.hstack([sorted_indices_big_to_small,nan_indices]))             # indices of biggest
+
+        print(scores[ordered_indices])
+        print ('ordered feats', ordered_indices.shape)
+        return ordered_indices
+
+def get_chi2_ordered_indices(all_training, training_labels):
+
+        # add values so that all features of training data are non-zero
+        min_value = np.min(all_training)
+        print(len(np.where(all_training<0)[0]))
+        print(min_value)
+        all_training=all_training-min_value
+        print(len(np.where(all_training < 0)[0]))
+
+
+        # get array of scores in the same order as original features
+        selector = SelectPercentile(chi2, percentile=100)
+        selector.fit(all_training, training_labels)
+        scores = selector.scores_
+
+        # sort the scores (small to big)
+        ordered_indices = np.argsort(scores)[::-1]
+        print(scores[ordered_indices])
+        print(ordered_indices)
+        return ordered_indices
+
+def get_mutinfo_ordered_indices(all_training,training_labels):
+        scores = mutual_info_classif(all_training, training_labels)
+        print('scores', len(scores))
+        print('scores', scores)
+
+        print('sorted scores',
+              scores[np.argsort(scores)[::-1]])  # argsort(scores) gives them from smallest to biggest - indexing reverses this
+        ordered_indices = np.array(np.argsort(scores)[::-1])  # ordered feats is np array of indices from biggest to smallest
+        print('ordered feats', ordered_indices.shape)
+        print('ordered feats', ordered_indices)
+        return ordered_indices
 
 
 def single_fold(k, topk, dataset, datasetnum, kernel, cmin, cmax, number_of_cs, skfseed, percent_of_priv, percentageofinstances, take_top_t, featsel):
@@ -46,7 +98,7 @@ def single_fold(k, topk, dataset, datasetnum, kernel, cmin, cmax, number_of_cs, 
 
 
         print('word',take_top_t)
-        output_directory = get_full_path(('Desktop/Privileged_Data/LUFeSubset-{}-10x10-{}-ALLCV{}to{}-featsscaled-step{}-{}percentinstances/{}{}/top{}chosen-{}percentinstances/').format(featsel, dataset, cmin, cmax, stepsize, percentageofinstances, dataset, datasetnum, topk, percentageofinstances))
+        output_directory = get_full_path(('Desktop/Privileged_Data/Subset-{}-10x10-{}-ALLCV{}to{}-featsscaled-step{}-{}percentinstances/{}{}/top{}chosen-{}percentinstances/').format(featsel, dataset, cmin, cmax, stepsize, percentageofinstances, dataset, datasetnum, topk, percentageofinstances))
         print (output_directory)
 
         try:
@@ -69,8 +121,6 @@ def single_fold(k, topk, dataset, datasetnum, kernel, cmin, cmax, number_of_cs, 
         all_training, all_testing, training_labels, testing_labels = get_train_and_test_this_fold(dataset,datasetnum,k,skfseed)
 
 
-
-
         n_top_feats = topk
 
         # n_top_feats = topk*all_training.shape[1]//100
@@ -80,63 +130,18 @@ def single_fold(k, topk, dataset, datasetnum, kernel, cmin, cmax, number_of_cs, 
 
         ######### UNIVARIATE PART
 
-        ## ANOVA
 
         if featsel == 'anova':
-                # print('shapes:',all_training.shape,all_testing.shape)
-                # selector = VarianceThreshold()
-                # all_data = np.vstack([all_training,all_testing])
-                # # selector.fit(all_data)
-                # selector.fit_transform(all_training)
-                # non_neg_indices = selector.get_support()
-                # neg_indices = np.invert(non_neg_indices)
-                #
-                #
-                # print((non_neg_indices))
-                # print((neg_indices))
-                # print(len(non_neg_indices))
-                # print(len(neg_indices))
-                #
-                # sys.exit()
-                #
-                # all_training=all_training[:,non_neg_indices]
-                # all_testing = all_testing[:, non_neg_indices]
-                #
-                #
-                # print('shapes:', all_training.shape, all_testing.shape)
-
-                selector = SelectPercentile(f_classif, percentile=100)
-                selector.fit(all_training, training_labels)
-                scores = selector.scores_
-                print('scores', len(scores))
-                print('scores', scores)
-
-
-
-                print('sorted scores', scores[np.argsort(scores)[::-1]][:5000])
-                ordered_feats = np.array(np.argsort(scores)[::-1])
-                # ordered_feats = np.array(np.argsort(scores))
-                print ('ordered feats', ordered_feats.shape)
-                print('ordered feats', ordered_feats)
-
-        ## MUTUAL INFO
-
+                ordered_indices = get_anova_ordered_indices(all_training,training_labels)
         if featsel == 'mutinfo':
-                scores = mutual_info_classif(all_training, training_labels)
-                print('scores', len(scores))
-                print('scores', scores)
-
-                # [::-1]
-
-
-                print('sorted scores',scores[np.argsort(scores)[::-1]]) # argsort(scores) gives them from smallest to biggest - indexing reverses this
-                ordered_feats = np.array(np.argsort(scores)[::-1])    # ordered feats is np array of indices from biggest to smallest
-                print('ordered feats', ordered_feats.shape)
-                print('ordered feats', ordered_feats)
+                ordered_indices = get_mutinfo_ordered_indices(all_training, training_labels)
+        if featsel == 'chi2':
+                ordered_indices = get_chi2_ordered_indices(all_training, training_labels)
+        # sys.exit()
 
 
-        sorted_training = all_training[:, ordered_feats]  # sort all instances' features from smallest to biggest
-        sorted_testing = all_testing[:, ordered_feats]
+        sorted_training = all_training[:, ordered_indices]
+        sorted_testing = all_testing[:, ordered_indices]
 
         normal_features_training = sorted_training[:, :n_top_feats]  # take the first n_top_feats
         normal_features_testing = sorted_testing[:, :n_top_feats]
@@ -145,59 +150,6 @@ def single_fold(k, topk, dataset, datasetnum, kernel, cmin, cmax, number_of_cs, 
         # n_top_feats = topk*all_training.shape[1]//100
         print('n top feats', n_top_feats)
         param_estimation_file.write("\n\n n={},fold={}".format(n_top_feats, k))
-
-
-        #
-        # best_C = get_best_C(normal_features_training, training_labels, c_values, cross_validation_folder, datasetnum,
-        #                     topk)
-        # clf = svm.SVC(C=best_C, kernel=kernel, random_state=k)
-        # clf.fit(normal_features_training, training_labels)
-        # # svm_predictions = clf.predict(normal_features_testing)
-        # # with open(os.path.join(cross_validation_folder, 'svm-{}-{}.csv'.format(k, topk)), 'a') as cv_svm_file:
-        # #     cv_svm_file.write(str(accuracy_score(testing_labels, svm_predictions)) + ',')
-
-        ########## GET BEST C FOR RFE
-        #
-        # # best_rfe_param = get_best_RFE_C(all_training,training_labels, c_values, n_top_feats,stepsize,cross_validation_folder,datasetnum,topk)
-        # best_rfe_param = get_best_RFE_C(all_training, training_labels, c_values, n_top_feats, stepsize,
-        #                                  datasetnum, topk)
-        # print('best rfe param', best_rfe_param)
-
-        ###########  CARRY OUT RFE, GET ACCURACY
-
-        # svc = SVC(C=best_rfe_param, kernel=kernel, random_state=k)
-        # rfe = RFE(estimator=svc, n_features_to_select=n_top_feats, step=stepsize)
-        # print ('rfe step size',rfe.step)
-        # rfe.fit(all_training, training_labels)
-        # print (all_testing.shape,testing_labels.shape)
-        # print ('num of chosen feats',sum(x == 1 for x in rfe.support_))
-        #
-        # best_n_mask = rfe.support_
-        # normal_features_training = all_training[:,best_n_mask].copy()
-        # normal_features_testing = all_testing[:,best_n_mask].copy()
-        # privileged_features_training=all_training[:,np.invert(rfe.support_)].copy()
-        #
-        # svc = SVC(C=best_rfe_param, kernel=kernel, random_state=k)
-        # svc.fit(normal_features_training,training_labels)
-        # rfe_accuracy = svc.score(normal_features_testing,testing_labels)
-        # print ('rfe accuracy (using slice):',rfe_accuracy)
-        #
-        #
-        # with open(os.path.join(cross_validation_folder,'svm-{}-{}.csv'.format(k,topk)),'a') as cv_svm_file:
-        #     cv_svm_file.write(str(rfe_accuracy)+",")
-        #
-        # print('normal train shape {},priv train shape {}'.format(normal_features_training.shape,privileged_features_training.shape))
-        # print('normal testing shape {}'.format(normal_features_testing.shape))
-
-
-
-        ############# SVM PLUS - PARAM ESTIMATION AND RUNNING
-        #
-        # all_features_ranking = rfe.ranking_[np.invert(best_n_mask)]
-        # privileged_features_training = privileged_features_training[:, np.argsort(all_features_ranking)]
-
-        ##### THIS PART TO GET A SUBSET OF PRIV INFO####
-
 
 
         for percent_of_priv in [100,10]:
@@ -218,14 +170,6 @@ def single_fold(k, topk, dataset, datasetnum, kernel, cmin, cmax, number_of_cs, 
                         privileged_features_training2 = privileged_features_training[:,-num_of_priv_feats:]
                 print ('privileged data shape',privileged_features_training2.shape)
 
-
-        ##### THIS PART TO USE RANDOM DATA AS PRIVILEGED
-        # privileged_features_training = get_random_array(privileged_features_training.shape[0],privileged_features_training.shape[1]*5)
-        # random_array = np.random.rand(privileged_features_training.shape[0],privileged_features_training.shape[1])
-        # random_array = preprocessing.scale(random_array)
-        # privileged_features_training=random_array
-        # print ('random data size',privileged_features_training.shape)
-        #################################
 
                 c_star_values=c_values
                 c_svm_plus,c_star_svm_plus = get_best_CandCstar(normal_features_training,training_labels, privileged_features_training2,
@@ -251,7 +195,7 @@ def single_fold(k, topk, dataset, datasetnum, kernel, cmin, cmax, number_of_cs, 
 
 # value = 1
 
-# single_fold(k=3, topk=500, dataset='tech', datasetnum=245, kernel='linear', cmin=-3, cmax=3, number_of_cs=7,skfseed=4, percent_of_priv=100, percentageofinstances=100, take_top_t='bottom', feat_sel='anova')
+# single_fold(k=3, topk=500, dataset='tech', datasetnum=245, kernel='linear', cmin=-3, cmax=3, number_of_cs=7,skfseed=4, percent_of_priv=100, percentageofinstances=100, take_top_t='bottom', featsel='chi2')
 
 # for dataset in ['madelon','gisette','dexter','dorothea']:
 #         for skfseed in range(10):
