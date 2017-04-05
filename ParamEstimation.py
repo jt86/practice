a06f310b13590a208dccf021191127a950a6a25e
Joe Taylor <jt306@sussex.ac.uk>
2017-04-05 17:32:12 +0100

    SingleFoldSlice broken into sub functions

--- ParamEstimation.py
+++ ParamEstimation.py
@@ -80,20 +80,21 @@ def get_best_params_dp(setting, normal_train, labels_train, priv_train, cross_va
     return best_C, best_gamma, best_delta
 
 
-def get_best_CandCstar(training_data,training_labels, privileged_data, c_values, Cstar_values,cross_validation_folder,datasetnum,topk):
+
+def get_best_CandCstar(s, normal_train, labels_train, priv_train, cross_val_folder2):
     n_folds=5
     # skf = cross_validation.StratifiedKFold(n_folds)
     # cv = skf.split(training_data, training_labels)
-    cv = StratifiedKFold(training_labels, n_folds=5, shuffle=True)
-    cv_scores = np.zeros((len(Cstar_values),len(c_values)))	#join cross validation on X, X*
+    cv = StratifiedKFold(labels_train, n_folds=5, shuffle=True)
+    cv_scores = np.zeros((len(s.cvalues),len(s.cvalues)))	#join cross validation on X, X*
     print ('cv scores shape',cv_scores.shape)
     for i,(train, test) in enumerate(cv):
-        for Cstar_index, Cstar in enumerate(Cstar_values):
-            for C_index, C in enumerate(c_values):
+        for Cstar_index, Cstar in enumerate(s.cvalues):
+            for C_index, C in enumerate(s.cvalues):
                 # print('cstar', Cstar,'c index',C_index,'c',C)
-                duals,bias = svmplusQP(training_data[train],training_labels[train].copy(),privileged_data[train],C,Cstar)
-                predictions = svmplusQP_Predict(training_data[train],training_data[test],duals,bias).flatten()
-                ACC = np.sum(training_labels[test]==np.sign(predictions))/(1.*len(training_labels[test]))
+                duals,bias = svmplusQP(normal_train[train], labels_train[train].copy(), priv_train[train], C, Cstar)
+                predictions = svmplusQP_Predict(normal_train[train], normal_train[test], duals, bias).flatten()
+                ACC = np.sum(labels_train[test] == np.sign(predictions)) / (1. * len(labels_train[test]))
                 cv_scores[Cstar_index,C_index] += ACC
                 sys.stdout.flush()
                 # print (cv_scores)
@@ -103,11 +104,11 @@ def get_best_CandCstar(training_data,training_labels, privileged_data, c_values,
     # index_of_best = best_positions[int(len(best_positions)/2)]
 
     print('index of best',index_of_best)
-    best_Cstar, best_C = Cstar_values[index_of_best[0]],c_values[index_of_best[1]]
-    with open(os.path.join(cross_validation_folder,'Cstar-crossvalid-{}-{}.txt'.format(datasetnum,topk)),'a') as cross_validation_doc:
+    best_Cstar, best_C = s.cvalues[index_of_best[0]],s.cvalues[index_of_best[1]]
+    with open(os.path.join(cross_val_folder2, 'Cstar-crossvalid-{}-{}.txt'.format(s.datasetnum, s.topk)), 'a') as cross_validation_doc:
         cross_validation_doc.write("\n{} => bestC={},bestC*={}".format(cv_scores,best_C,best_Cstar))
-    print('c* values:',Cstar_values)
-    print('c values:',c_values)
+    print('c* values:',s.cvalues)
+    print('c values:',s.cvalues)
     print('cross valid scores:\n',cv_scores,'=> best C*=',best_Cstar, 'bestC=',best_C)
     return best_C, best_Cstar
 
@@ -133,37 +134,38 @@ def get_best_CandCstar(training_data,training_labels, privileged_data, c_values,
 #     print('cross valid scores:',cv_scores,'=> best C*=',best_Cstar)
 #     return best_Cstar
 
-def get_best_C(training_data,training_labels, c_values, cross_validation_folder,datasetnum,topk):
+def get_best_C(s, all_train, labels_train, cross_val_folder):
     # skf = cross_validation.StratifiedKFold(5)
     # cv = skf.split(training_data,training_labels)
-    cv =StratifiedKFold(training_labels,n_folds=5,shuffle=True)
-    cv_scores = np.zeros(len(c_values))
+    cv =StratifiedKFold(labels_train, n_folds=5, shuffle=True)
+    cv_scores = np.zeros(len(s.cvalues))
     for i,(train, test) in enumerate(cv):
-        for C_index, C in enumerate(c_values):
+        for C_index, C in enumerate(s.cvalues):
             # print('c index',C_index,'c',C)
             svc = SVC(C=C, kernel="linear", random_state=1)
-            svc.fit(training_data[train], training_labels[train])
-            cv_scores[C_index] += svc.score(training_data[test], training_labels[test])
+            svc.fit(all_train[train], labels_train[train])
+            cv_scores[C_index] += svc.score(all_train[test], labels_train[test])
             sys.stdout.flush()
     cv_scores = cv_scores/5.
     best_positions = (np.argwhere(cv_scores.max() == cv_scores))
     index_of_best=best_positions[0]
     # index_of_best = best_positions[int(len(best_positions)/2)]
-    best_C = c_values[index_of_best][0]
-    with open(os.path.join(cross_validation_folder,'C_fullset-crossvalid-{}-{}.txt'.format(datasetnum,topk)),'a') as cross_validation_doc:
+    best_C = s.cvalues[index_of_best][0]
+    with open(os.path.join(cross_val_folder,'C_fullset-crossvalid-{}-{}.txt'.format(s.datasetnum,s.topk)),'a') as cross_validation_doc:
         cross_validation_doc.write("\n{} {}".format(cv_scores,best_C))
     print('cross valid scores (all features):',cv_scores,'=> best C=',best_C)
     return best_C
 
-    best_rfe_param = get_best_RFE_C(all_training, labels_train, s.cvalues, s.topk, stepsize)
+    # best_rfe_param = get_best_RFE_C(all_training, labels_train, s.cvalues, s.topk, stepsize)
 
-def get_best_RFE_C(setting,training_data,training_labels,stepsize):
+
+def get_best_RFE_C(setting, all_train, labels_train):
 
     starttime = time.clock()
     # print ('time', starttime)
     # skf = cross_validation.StratifiedKFold(5)
     # cv = skf.split(training_data, training_labels)
-    cv = StratifiedKFold(training_labels, n_folds=5, shuffle=True)
+    cv = StratifiedKFold(labels_train, n_folds=5, shuffle=True)
     cv_scores = numpy.zeros(len(setting.cvalues))
 
     for i,(train, test) in enumerate(cv):
@@ -171,9 +173,9 @@ def get_best_RFE_C(setting,training_data,training_labels,stepsize):
         for C_index, C in enumerate(setting.cvalues):
             # print('c index',C_index,'c',C)
             svc = SVC(C=C, kernel="linear", random_state=1)
-            rfe = RFE(estimator=svc, n_features_to_select=setting.topk, step=stepsize)
-            rfe.fit(training_data[train], training_labels[train])
-            cv_scores[C_index] += rfe.score(training_data[test], training_labels[test])
+            rfe = RFE(estimator=svc, n_features_to_select=setting.topk, step=setting.stepsize)
+            rfe.fit(all_train[train], labels_train[train])
+            cv_scores[C_index] += rfe.score(all_train[test], labels_train[test])
             sys.stdout.flush()
         # print 'fold',i,cv_scores
 
--- SingleFoldSlice.py
+++ SingleFoldSlice.py
@@ -63,8 +63,7 @@ def delta_plus(s, normal_train, labels_train, priv_train, normal_test, labels_te
         cv_lupi_file.write(str(dp_score) + ',')
 
 def svm_plus(s, normal_train, labels_train, priv_train, normal_test, labels_test, cross_val_folder2):
-    c_svm_plus, c_star_svm_plus = get_best_CandCstar(s, normal_train, labels_train, priv_train, cross_val_folder2,
-                                                     s.datasetnum, s.topk)
+    c_svm_plus, c_star_svm_plus = get_best_CandCstar(s, normal_train, labels_train, priv_train, cross_val_folder2)
     duals, bias = svmplusQP(normal_train, labels_train.copy(), priv_train, c_svm_plus, c_star_svm_plus)
     lupi_predictions = svmplusQP_Predict(normal_train, normal_test, duals, bias).flatten()
     accuracy_lupi = np.sum(labels_test == np.sign(lupi_predictions)) / (1. * len(labels_test))
@@ -72,95 +71,78 @@ def svm_plus(s, normal_train, labels_train, priv_train, normal_test, labels_test
         cv_lupi_file.write(str(accuracy_lupi) + ',')
 
 
-def do_lufe(s, normal_train, labels_train, priv_train, normal_test, labels_test, cross_val_folder2):
+def do_lufe(s, normal_train, labels_train, priv_train, normal_test, labels_test, cross_val_folder):
+    cross_val_folder2 = os.path.join(cross_val_folder, '{}-{}'.format(s.take_top_t, s.percent_of_priv))
+    make_directory(cross_val_folder2)
+    priv_train = get_priv_subset(priv_train, s.take_top_t, s.percent_of_priv)
+
     if s.lupimethod == 'dp':
         delta_plus(s, normal_train, labels_train, priv_train, normal_test, labels_test, cross_val_folder2)
     else:
-        (s, normal_train, labels_train, priv_train, normal_test, labels_test, cross_val_folder2)
+        svm_plus(s, normal_train, labels_train, priv_train, normal_test, labels_test, cross_val_folder2)
+
+def do_rfe(s, all_train, all_test, labels_train, labels_test,cross_val_folder):
+    best_rfe_param = get_best_RFE_C(s, all_train, labels_train)
+    svc = SVC(C=best_rfe_param, kernel=s.kernel, random_state=s.k)
+    rfe = RFE(estimator=svc, n_features_to_select=s.topk, step=s.stepsize)
+    rfe.fit(all_train, labels_train)
+    best_n_mask = rfe.support_
+    normal_train = all_train[:, best_n_mask].copy()
+    normal_test = all_test[:, best_n_mask].copy()
+    priv_train = all_train[:, np.invert(rfe.support_)].copy()
+    np.save(get_full_path('Desktop/Privileged_Data/SavedIndices/top{}RFE/{}{}-{}-{}'.
+                          format(setting.topk, s.dataset, s.datasetnum, s.skfseed, s.k)), best_n_mask)
+    return normal_train, normal_test, priv_train
+
+def use_rfe(s, normal_train, priv_train, labels_train, normal_test, labels_test, best_rfe_param, cross_val_folder):
+    svc = SVC(C=best_rfe_param, kernel=s.kernel, random_state=s.k)
+    svc.fit(normal_train, labels_train)
+    rfe_accuracy = svc.score(normal_test, labels_test)
+    all_features_ranking = rfe.ranking_[np.invert(best_n_mask)]
+    priv_train = priv_train[:, np.argsort(all_features_ranking)]
+    with open(os.path.join(cross_val_folder, 'svm-{}-{}.csv'.format(s.k, s.topk)), 'a') as cv_svm_file:
+        cv_svm_file.write(str(rfe_accuracy) + ",")
 
 
+
+
+
+
+
+def do_baseline(s, all_train, labels_train, all_test, labels_test, cross_val_folder):
+    best_C_baseline = get_best_C(s, all_train, labels_train, cross_val_folder)
+    print('all training shape', all_train.shape)
+    clf = svm.SVC(C=best_C_baseline, kernel=setting.kernel, random_state=setting.k)
+    clf.fit(all_train, labels_train)
+    baseline_predictions = clf.predict(all_test)
+    print('baseline', accuracy_score(labels_test, baseline_predictions))
+    with open(os.path.join(cross_val_folder, 'baseline-{}.csv'.format(setting.k)), 'a') as baseline_file:
+        baseline_file.write(str(accuracy_score(labels_test, baseline_predictions)) + ',')
+
+
+
+##################################################################################################
 
 def single_fold(s):
-    print('using  {}% of training data instances'.format(s.percentageofinstances))
-    # print('percentage of discarded info used as priv:{}'.format(percent_of_priv))
-    stepsize = 0.1
+    print('{}% of train instances; {}% of discarded feats used as priv'.format(s.percentageofinstances,s.percent_of_priv))
     np.random.seed(s.k)
     output_directory = get_full_path((
                                      'Desktop/Privileged_Data/PRACTICELUFe-SVMdelta-10x10-{}-ALLCV-3to3-featsscaled-step{}-{}percentinstances/{}{}/top{}chosen-{}percentinstances/').format(
-        s.dataset, stepsize, s.percentageofinstances, s.dataset, s.datasetnum, s.topk, s.percentageofinstances))
-    print(output_directory)
+        s.dataset, s.stepsize, s.percentageofinstances, s.dataset, s.datasetnum, s.topk, s.percentageofinstances))
+
     make_directory(output_directory)
     param_estimation_file = open(os.path.join(output_directory, 'param_selection.csv'), "a")
     cross_val_folder = os.path.join(output_directory, 'cross-validation{}'.format(s.skfseed))
     make_directory(cross_val_folder)
 
-    all_training, all_testing, labels_train, labels_test = get_train_and_test_this_fold(s)
+    all_train, all_test, labels_train, labels_test = get_train_and_test_this_fold(s)
     param_estimation_file.write("\n\n n={},fold={}".format(s.topk, s.k))
 
 
-    ########## GET BEST C FOR RFE
-
-    # best_rfe_param = get_best_RFE_C(all_training,labels_train, c_values, setting.topk,stepsize,cross_val_folder,datasetnum,topk)
-    best_rfe_param = get_best_RFE_C(s, all_training, labels_train,stepsize)
-    print('best rfe param', best_rfe_param)
-
-    ###########  CARRY OUT RFE, GET ACCURACY
-
-    svc = SVC(C=best_rfe_param, kernel=s.kernel, random_state=s.k)
-    rfe = RFE(estimator=svc, n_features_to_select=s.topk, step=stepsize)
-    print('rfe step size', rfe.step)
-    rfe.fit(all_training, labels_train)
-    print(all_testing.shape, labels_test.shape)
-    print('num of chosen feats', sum(x == 1 for x in rfe.support_))
-
-    best_n_mask = rfe.support_
-    normal_train = all_training[:, best_n_mask].copy()
-    normal_test = all_testing[:, best_n_mask].copy()
-    priv_train = all_training[:, np.invert(rfe.support_)].copy()
-
-    svc = SVC(C=best_rfe_param, kernel=s.kernel, random_state=s.k)
-    svc.fit(normal_train, labels_train)
-    rfe_accuracy = svc.score(normal_test, labels_test)
-    print('rfe accuracy (using slice):', rfe_accuracy)
-
-    # np.save(get_full_path('Desktop/Privileged_Data/SavedIndices/top{}RFE/{}{}-{}-{}'.format(setting.topk,dataset,datasetnum,skfseed,k)),best_n_mask)
-
-
-    # with open(os.path.join(cross_val_folder, 'svm-{}-{}.csv'.format(k, setting.topk)), 'a') as cv_svm_file:
-    #     cv_svm_file.write(str(rfe_accuracy) + ",")
-    #
-    # print('normal train shape {},priv train shape {}'.format(normal_train.shape, priv_train.shape))
-    # print('normal testing shape {}'.format(normal_test.shape))
-
-    ##############################  BASELINE - all features
-
-    # best_C_baseline = get_best_C(all_training, labels_train, c_values, cross_val_folder, setting.datasetnum, setting.topk)
-    #
-    # print('all training shape', all_training.shape)
-    #
-    # clf = svm.SVC(C=best_C_baseline, kernel=setting.kernel, random_state=k)
-    # clf.fit(all_training, labels_train)
-    # baseline_predictions = clf.predict(all_testing)
-    # print('baseline', accuracy_score(labels_test, baseline_predictions))
-    #
-    # with open(os.path.join(cross_val_folder, 'baseline-{}.csv'.format(setting.k)), 'a') as baseline_file:
-    #     baseline_file.write(str(accuracy_score(labels_test, baseline_predictions)) + ',')
-
-    ############# SVM PLUS - PARAM ESTIMATION AND RUNNING
-
-    all_features_ranking = rfe.ranking_[np.invert(best_n_mask)]
-    priv_train = priv_train[:, np.argsort(all_features_ranking)]
-
-    ##### THIS PART TO GET A SUBSET OF PRIV INFO####
-
-
-    cross_val_folder2 = os.path.join(cross_val_folder, '{}-{}'.format(s.take_top_t, s.percent_of_priv))
-    make_directory(cross_val_folder2)
-    priv_train = get_priv_subset(priv_train, s.take_top_t, s.percent_of_priv)
-
-    #################################
-
-    do_lufe(s, normal_train, labels_train, priv_train, normal_test, labels_test, cross_val_folder2)
+    # RFE returns which feature are normal, and which are priv
+    normal_train, normal_test, priv_train = do_rfe(s,all_train,all_test,labels_train,labels_test,cross_val_folder)
+    do_baseline(s, all_train, labels_train, all_test, labels_test, cross_val_folder)
+    do_lufe(s, normal_train, labels_train, priv_train, normal_test, labels_test, cross_val_folder)
 
 
 def get_random_array(num_instances, num_feats):
@@ -168,16 +150,6 @@ def get_random_array(num_instances, num_feats):
     random_array = preprocessing.scale(random_array)
     return random_array
 
-
-# for dataset in ['madelon','gisette','dexter','dorothea']:
-#         for skfseed in range(10):
-#             for k in range(10):
-#                     single_fold(k=k, topk=300, dataset=dataset, datasetnum=None, kernel='linear', cmin=-3, cmax=3, number_of_cs=7,skfseed=skfseed, percent_of_priv=100, percentageofinstances=100,take_top_t='top')
-#
-
-
-
-# print(single_fold(k=0, topk=5000, dataset='awa', datasetnum=0, kernel='linear', cmin=-3, cmax=3, number_of_cs=4,skfseed=9, percent_of_priv=100, percentageofinstances=100,take_top_t='top'))
 
 
 ####### This part takes a subset of training instances
@@ -207,7 +179,8 @@ class Experiment_Setting:
         self.percentageofinstances = percentageofinstances
         self.take_top_t = take_top_t
         self.lupimethod =lupimethod
-        print('blah',self.cvalues)
+        self.stepsize=0.1
+
 
 
 setting = Experiment_Setting(k=3, topk=300, dataset='tech', datasetnum=245, kernel='linear', cvalues=[-3,3,1], skfseed=4,
