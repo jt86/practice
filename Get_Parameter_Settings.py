010440c01af4016ae50151321c9d2c26d29e2522
Joe Taylor <jt306@sussex.ac.uk>
2017-04-14 11:24:30 +0100

    Running experiments over Easter: Mutual info deltaplus

--- Get_Parameter_Settings.py
+++ Get_Parameter_Settings.py
@@ -23,7 +23,7 @@ for lupimethod in ['dp','svmplus']:
     for featsel in ['RFE']:
         for fold_num in range(10):
             for datasetnum in range (295): #5
-                print('--k {} --topk {} --dataset {} --datasetnum {} --kernel {} --cvalues {} --skfseed {} --percentofpriv {} --percentageofinstances {} --taketopt {} --lupimethod {} --featsel {}'.format(fold_num, top_k, dataset, datasetnum, 'linear', cvalues, seed, percentofpriv, 100, take_top_t, lupimethod, featsel))
+                print('--k {} --topk {} --dataset {} --datasetnum {} --kernel {} --cmin -3 --cmax 3 --numberofcs 7 --skfseed {} --percentofpriv {} --percentageofinstances {} --taketopt {} --lupimethod {} --featsel {}'.format(fold_num, top_k, dataset, datasetnum, 'linear', cvalues, seed, percentofpriv, 100, take_top_t, lupimethod, featsel))
 #                 count+=1
 # print(count)
 
