cb9ea0643099020f04d065837d940dc6a1245274
Joe Taylor <jt306@sussex.ac.uk>
2017-04-25 15:56:00 +0100

    Running experiments over Easter: Mutual info deltaplus

--- RunExperiment.py
+++ RunExperiment.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--skfseed', type=int, required=True, help='seed for random division of SKF - to allow 10x10fold')
 
-    parser.add_argument('--percentofpriv', required=True, help='percentage of priv info to take')
+    parser.add_argument('--percentofpriv', type=int, required=True, help='percentage of priv info to take')
 
     parser.add_argument('--percentageofinstances', type=int, required=True, help='percentage of training instances used')
 
