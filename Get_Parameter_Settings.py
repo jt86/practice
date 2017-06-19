88eca82389d7efe295ece4eefd38b3f4e8dca7ac
Joe Taylor <jt306@sussex.ac.uk>
2017-06-19 15:59:25 +0100

    Running experiments again - LUFe with RFE/ANOVA/Chi2/MI .. SVM+ and delta+. Then BAHSIC.

--- Get_Parameter_Settings.py
+++ Get_Parameter_Settings.py
@@ -34,7 +34,7 @@ classifier = 'lufe'
 foldnum=0
 datasetnum=0
 for featsel in ['rfe','anova', 'chi2','mi']:
-    for lupimethod in ['svmpplus']:
+    for lupimethod in ['svmplus']:
         print(
             '--foldnum {} --topk {} --dataset {} --datasetnum {} --skfseed {} --lupimethod {} --featsel {} --classifier {} --kernel linear  --cmin -3 --cmax 3 --numberofcs 7 --percentofpriv 100 --percentageofinstances 100 --taketopt top'
             .format(foldnum, top_k, dataset, datasetnum, seed, lupimethod, featsel, classifier))
