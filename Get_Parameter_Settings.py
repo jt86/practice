1630d428fa1c61adb1a70f617f163fe2e7830010
Joe Taylor <jt306@sussex.ac.uk>
2017-07-07 18:45:13 +0100

    Running experiments again - lufereverse; svmreverse

--- Get_Parameter_Settings.py
+++ Get_Parameter_Settings.py
@@ -47,7 +47,7 @@ for featsel in ['rfe','bahsic','anova','chi2','mi']:
     for foldnum in range(10):
         for datasetnum in range(295):  # 5
             print('--foldnum {} --topk {} --dataset {} --datasetnum {} --skfseed {} --lupimethod {} --featsel {} --classifier {} --kernel linear  --cmin -3 --cmax 3 --numberofcs 7 --percentofpriv 100 --percentageofinstances 100 --taketopt top'
-                .format(foldnum, top_k, dataset, datasetnum, seed, lupimethod, featsel, classifier))
+                .format(foldnum, top_k, dataset, datasetnum, seed, 'nolufe', featsel, classifier))
             count+=1
 # print(count)
 
