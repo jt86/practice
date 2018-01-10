96f098940a940a98763fd59a5868cfcfc537f992
Joe Taylor <jt306@sussex.ac.uk>
2018-01-10 12:05:31 +0000

    Multi-Task Learning - running on cluster

--- practice.py
+++ practice.py
@@ -1,3 +1,5 @@
 import numpy as np
-
-print(np.shape(np.load('/Volumes/LocalDataHD/j/jt/jt306/Desktop/Privileged_Data/JuneResults2/lufe-dsvm-mi-300selected-top100priv/tech209/dvalues/tech209-1-7.npy')))
+import math
+print(8/3)
+print(math.floor(8/3))
+print(8//3)
