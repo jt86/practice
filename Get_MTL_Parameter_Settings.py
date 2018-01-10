1835972c9ef630fd9899e1d01ded03ccd5c94352
Joe Taylor <jt306@sussex.ac.uk>
2018-01-10 11:12:27 +0000

    Multi-Task Learning - running on cluster

--- Get_MTL_Parameter_Settings.py
+++ Get_MTL_Parameter_Settings.py
@@ -8,7 +8,7 @@ count = 1
 
 for foldnum in range(10):
     for num_unsel_feats in [1,2,3]+[item for item in range(10,300,10)]+[item for item in range (1000,2200,100)]+['all']:
-        print('--numhiddenunits {} --rate {} --weight {} --featselector {} foldnum {} --numunselfeats {}'\
+        print('--numhiddenunits {} --rate {} --weight {} --featselector {} --foldnum {} --numunselfeats {}'\
         .format(num_hidden_units, rate, weight, featsel, foldnum, num_unsel_feats))
         count+=1
 print (count)
